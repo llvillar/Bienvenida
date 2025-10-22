d672b9608044dd030bca9b191a472fb11098b6a8
Luis Lopez <llvillar@gmail.com>
2025-10-22 18:51:23 +0200

    vv

--- Bienvenida/src/es/iesoretania/saludo/Adios.java
+++ Bienvenida/src/es/iesoretania/saludo/Adios.java
@@ -4,7 +4,7 @@ public class Adios {
 
 	public static void main(String[] args) {
 
-		System.out.println("Adios mundo!!!!!!!");
+		System.out.println("Adios muntdo!!!!!!!");
 
 		System.out.println("Adios mundo!!!!!!!");
 
